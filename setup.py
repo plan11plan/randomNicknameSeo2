393a60f89c03449ffd78daeb7345c9f6bfb423e1
kimjinsu <tjtfy7028@gmail.com>
2024-04-21 17:25:55 +0900

    1

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='randomNicknameSeo2',
+    version='0.0.1',
+    description='randomNicknameSeo2',
+    author='jins',
+    url='https://github.com/plan11plan/simple',
+    download_url='https://github.com/plan11plan/simple',
+    packages=['test'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+
+)
